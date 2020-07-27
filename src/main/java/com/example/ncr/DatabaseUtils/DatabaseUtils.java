9c229dcb25e9c046a4ad86d208319352b65de5e6
sb185460 <sb185460@ncr.com>
2020-07-28 04:19:49 +0530

    re-profile

--- src/main/java/com/example/ncr/DatabaseUtils/DatabaseUtils.java
+++ src/main/java/com/example/ncr/DatabaseUtils/DatabaseUtils.java
@@ -1,4 +1,4 @@
-package database;
+package com.example.ncr.DatabaseUtils;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
--- src/main/java/com/example/ncr/services/ProfileController.java
+++ src/main/java/com/example/ncr/services/ProfileController.java
@@ -1,24 +1,16 @@
 package com.example.ncr.services;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.ncr.DatabaseUtils.DatabaseUtils;
 import com.google.api.core.ApiFuture;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.FirestoreOptions;
-
-import database.DatabaseUtils;
 
 @RestController
 public class ProfileController {
