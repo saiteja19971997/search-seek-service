0fe313e03d5790f56f6b32f94d786262f5b90fb3
sb185460 <sb185460@ncr.com>
2020-07-27 12:20:18 +0530

    first commit

---
+++ src/main/java/com/example/ncr/SearchSeekServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ncr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchSeekServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchSeekServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ncr/SearchSeekServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ncr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchSeekServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
